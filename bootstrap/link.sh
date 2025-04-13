28facb581a8894e0b90836e0aaa9a1f27072db89
Shota Ejima <eji@websci.cs.tsukuba.ac.jp>
2025-04-13 17:18:03 +0900

    add vscode settings json

--- bootstrap/link.sh
+++ bootstrap/link.sh
@@ -4,3 +4,4 @@
 mkdir -p $HOME/.config/git
 ln -fnsv $(pwd)/git/ignore $HOME/.config/git/ignore
 ln -fnsv $(pwd)/git/.gitconfig $HOME/.gitconfig
+ln -fnsv $(pwd)/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
